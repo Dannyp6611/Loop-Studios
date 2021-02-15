0031e4b6d246b9a36fb05d079a34591c08502a78
Dannyp6611 <54036518+Dannyp6611@users.noreply.github.com>
2021-02-15 20:02:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+
+    document.querySelector(".nav__links").classList.toggle("open");
+
+});
